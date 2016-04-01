5c77a238e487e0f2d85964eabf08e7cccb5bc96f
Simon Howroyd <howroyd@users.noreply.github.com>
2016-04-01 11:26:43 +0100

    Added parser

--- main.py
+++ main.py
@@ -7,10 +7,13 @@
 #############################################################################
 
 import time
+import serial
 
 class Controller():
     def __init__(self):
+        self.__raw_frame = ''
         self.__my_frame = {
+            'timestamp' : '0',
             'RedLed' : 'on',
             'GrnLed' : 'on',
             'CartPwr' : 'on',
@@ -26,7 +29,7 @@ class Controller():
             'AdcTrigger' : '100',
             'PurgeValve' : '0',
             'InletValve' : '0',
-            'OutDaq' : 'en',
+            'OutDaq' : 'en',  # Disable controller
             'InDaq' : 'en',
             'DataDump' : '500',
             'VStkA1' : False,
@@ -51,48 +54,119 @@ class Controller():
 
     def get_frame(self):
         x = self.__my_frame
-        out = 'STX,' + str(time.time())
-        out = out + ',' + str(x['RedLed'])
-        out = out + ',' + str(x['GrnLed'])
-        out = out + ',' + str(x['CartPwr'])
-        out = out + ',' + str(x['StkA'])
-        out = out + ',' + str(x['StkB'])
-        out = out + ',' + str(x['McuWake'])
-        out = out + ',' + str(x['HydStkA'])
-        out = out + ',' + str(x['HydStkB'])
-        out = out + ',' + str(x['TempStkA'])
-        out = out + ',' + str(x['TempStkB'])
-        out = out + ',' + str(x['Fan1'])
-        out = out + ',' + str(x['Fan2'])
-        out = out + ',' + str(x['AdcTrigger'])
-        out = out + ',' + str(x['PurgeValve'])
-        out = out + ',' + str(x['InletValve'])
-        out = out + ',' + str(x['OutDaq'])
-        out = out + ',' + str(x['InDaq'])
-        out = out + ',' + str(x['DataDump'])
-        out = out + ',' + str(x['VStkA1'])
-        out = out + ',' + str(x['IStkA1'])
-        out = out + ',' + str(x['VStkA2'])
-        out = out + ',' + str(x['IStkA2'])
-        out = out + ',' + str(x['VStkA3'])
-        out = out + ',' + str(x['IStkA3'])
-        out = out + ',' + str(x['VStkB1'])
-        out = out + ',' + str(x['IStkB1'])
-        out = out + ',' + str(x['VStkB2'])
-        out = out + ',' + str(x['IStkB2'])
-        out = out + ',' + str(x['VStkB3'])
-        out = out + ',' + str(x['IStkB3'])
-        out = out + ',' + str(x['VBatIn'])
-        out = out + ',' + str(x['IBatIn'])
-        out = out + ',' + str(x['VBatOut'])
-        out = out + ',' + str(x['IBatOut'])
-        out = out + ',' + str(x['VLoad'])
-        out = out + ',' + str(x['ILoad'])
+        out = 'STX'
+        out += ',' + str(x['timestamp'])
+        out += ',' + str(x['RedLed'])
+        out += ',' + str(x['GrnLed'])
+        out += ',' + str(x['CartPwr'])
+        out += ',' + str(x['StkA'])
+        out += ',' + str(x['StkB'])
+        out += ',' + str(x['McuWake'])
+        out += ',' + str(x['HydStkA'])
+        out += ',' + str(x['HydStkB'])
+        out += ',' + str(x['TempStkA'])
+        out += ',' + str(x['TempStkB'])
+        out += ',' + str(x['Fan1'])
+        out += ',' + str(x['Fan2'])
+        out += ',' + str(x['AdcTrigger'])
+        out += ',' + str(x['PurgeValve'])
+        out += ',' + str(x['InletValve'])
+        out += ',' + str(x['OutDaq'])
+        out += ',' + str(x['InDaq'])
+        out += ',' + str(x['DataDump'])
+        out += ',' + str(x['VStkA1'])
+        out += ',' + str(x['IStkA1'])
+        out += ',' + str(x['VStkA2'])
+        out += ',' + str(x['IStkA2'])
+        out += ',' + str(x['VStkA3'])
+        out += ',' + str(x['IStkA3'])
+        out += ',' + str(x['VStkB1'])
+        out += ',' + str(x['IStkB1'])
+        out += ',' + str(x['VStkB2'])
+        out += ',' + str(x['IStkB2'])
+        out += ',' + str(x['VStkB3'])
+        out += ',' + str(x['IStkB3'])
+        out += ',' + str(x['VBatIn'])
+        out += ',' + str(x['IBatIn'])
+        out += ',' + str(x['VBatOut'])
+        out += ',' + str(x['IBatOut'])
+        out += ',' + str(x['VLoad'])
+        out += ',' + str(x['ILoad'])
         return out + ',ETX,'
+        
+        
+    def parse_frame(__port):
+        raw = __port.read()
+        __full_frame = False
+        
+        try:
+            ptr = raw.index('STX') ######todo timestamp
+            self.__raw_frame = raw[ptr:]
+        except ValueError:
+            # STX not found
+            try:
+                ptr = raw.index('ETX') + 3
+                self.__raw_frame += raw[:ptr]
+                __full_frame = True
+            except ValueError:
+                # No identifiers found, middle of frame
+                self.__raw_frame += raw
+            
+        if __full_frame:
+            __split_frame = self.__raw_frame.split(',')
+            __full_frame = False
+            if (len(__split_frame) is 39:
+                # Full frame received!
+                self.__my_frame['timestamp'] = __split_frame[1]
+                self.__my_frame['RedLed'] = __split_frame[2]
+                self.__my_frame['GrnLed'] = __split_frame[3]
+                self.__my_frame['CartPwr'] = __split_frame[4]
+                self.__my_frame['StkA'] = __split_frame[5]
+                self.__my_frame['StkB'] = __split_frame[6]
+                self.__my_frame['McuWake'] = __split_frame[7]
+                self.__my_frame['HydStkA'] = __split_frame[8]
+                self.__my_frame['HydStkB'] = __split_frame[9]
+                self.__my_frame['TempStkA'] = __split_frame[10]
+                self.__my_frame['TempStkB'] = __split_frame[1]
+                self.__my_frame['Fan1'] = __split_frame[12]
+                self.__my_frame['Fan2'] = __split_frame[13]
+                self.__my_frame['AdcTrigger'] = __split_frame[14]
+                self.__my_frame['PurgeValve'] = __split_frame[15]
+                self.__my_frame['InletValve'] = __split_frame[16]
+                self.__my_frame['OutDaq'] = __split_frame[17]
+                self.__my_frame['InDaq'] = __split_frame[18]
+                self.__my_frame['DataDump'] = __split_frame[19]
+                self.__my_frame['VStkA1'] = __split_frame[20]
+                self.__my_frame['IStkA1'] = __split_frame[21]
+                self.__my_frame['VStkA2'] = __split_frame[22]
+                self.__my_frame['IStkA2'] = __split_frame[23]
+                self.__my_frame['VStkA3'] = __split_frame[24]
+                self.__my_frame['IStkA3'] = __split_frame[25]
+                self.__my_frame['VStkB1'] = __split_frame[26]
+                self.__my_frame['IStkB1'] = __split_frame[27]
+                self.__my_frame['VStkB2'] = __split_frame[28]
+                self.__my_frame['IStkB2'] = __split_frame[29]
+                self.__my_frame['VStkB3'] = __split_frame[30]
+                self.__my_frame['IStkB3'] = __split_frame[31]
+                self.__my_frame['VBatIn'] = __split_frame[32]
+                self.__my_frame['IBatIn'] = __split_frame[33]
+                self.__my_frame['VBatOut'] = __split_frame[34]
+                self.__my_frame['IBatOut'] = __split_frame[35]
+                self.__my_frame['VLoad'] = __split_frame[36]
+                self.__my_frame['ILoad'] = __split_frame[37]
+                return True
+            else:
+                # Corrupt frame
+                self.__raw_frame = ''
+        return False
 
 a=Controller()
+port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+log = open(("/media/usb/" + time.strftime("%y%m%d-%H%M%S") + "-AlexIE-" + ".tsv"), 'w')
 
 # Main run function
 if __name__ == "__main__":
-    print(a.get_frame())
+    if a.parse_frame(port):
+        log.write(a.get_frame())
+        log.write("\n")
 
